8eb4e6d753da4d6809701ac8649932caa9578e3d
dims94 <dimosmera@gmail.com>
2015-10-31 20:13:05 +0200

    Starting the main window
    Menu items need work
    Need of button images
    Listview needs work too

--- OpenMassSender/OpenMassSenderGUI/Form1.Designer.cs
+++ OpenMassSender/OpenMassSenderGUI/Form1.Designer.cs
@@ -28,12 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControlMainWindow = new System.Windows.Forms.TabControl();
+            this.tabPageStatus = new System.Windows.Forms.TabPage();
+            this.tabPageMessage = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemJobs = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemOptions = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.btnDelete = new System.Windows.Forms.ToolStripButton();
+            this.btnShowNotReady = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.labelNumber = new System.Windows.Forms.ToolStripLabel();
+            this.labelName = new System.Windows.Forms.ToolStripLabel();
+            this.labelStatus = new System.Windows.Forms.ToolStripLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControlMainWindow.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(565, 484);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip2);
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControlMainWindow);
+            this.splitContainer1.Size = new System.Drawing.Size(565, 484);
+            this.splitContainer1.SplitterDistance = 362;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tabControlMainWindow
+            // 
+            this.tabControlMainWindow.Controls.Add(this.tabPageStatus);
+            this.tabControlMainWindow.Controls.Add(this.tabPageMessage);
+            this.tabControlMainWindow.Location = new System.Drawing.Point(4, 4);
+            this.tabControlMainWindow.Name = "tabControlMainWindow";
+            this.tabControlMainWindow.SelectedIndex = 0;
+            this.tabControlMainWindow.Size = new System.Drawing.Size(561, 114);
+            this.tabControlMainWindow.TabIndex = 0;
+            // 
+            // tabPageStatus
+            // 
+            this.tabPageStatus.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStatus.Name = "tabPageStatus";
+            this.tabPageStatus.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStatus.Size = new System.Drawing.Size(553, 88);
+            this.tabPageStatus.TabIndex = 0;
+            this.tabPageStatus.Text = "Status";
+            this.tabPageStatus.UseVisualStyleBackColor = true;
+            // 
+            // tabPageMessage
+            // 
+            this.tabPageMessage.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMessage.Name = "tabPageMessage";
+            this.tabPageMessage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMessage.Size = new System.Drawing.Size(553, 88);
+            this.tabPageMessage.TabIndex = 1;
+            this.tabPageMessage.Text = "Message";
+            this.tabPageMessage.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemFile,
+            this.menuItemJobs,
+            this.menuItemOptions});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(565, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuItemFile
+            // 
+            this.menuItemFile.Name = "menuItemFile";
+            this.menuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.menuItemFile.Text = "File";
+            // 
+            // menuItemJobs
+            // 
+            this.menuItemJobs.Name = "menuItemJobs";
+            this.menuItemJobs.Size = new System.Drawing.Size(42, 20);
+            this.menuItemJobs.Text = "Jobs";
+            // 
+            // menuItemOptions
+            // 
+            this.menuItemOptions.Name = "menuItemOptions";
+            this.menuItemOptions.Size = new System.Drawing.Size(61, 20);
+            this.menuItemOptions.Text = "Options";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdd,
+            this.btnDelete,
+            this.btnShowNotReady});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(565, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(33, 22);
+            this.btnAdd.Text = "Add";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(44, 22);
+            this.btnDelete.Text = "Delete";
+            // 
+            // btnShowNotReady
+            // 
+            this.btnShowNotReady.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnShowNotReady.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnShowNotReady.Image = ((System.Drawing.Image)(resources.GetObject("btnShowNotReady.Image")));
+            this.btnShowNotReady.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnShowNotReady.Name = "btnShowNotReady";
+            this.btnShowNotReady.Size = new System.Drawing.Size(92, 22);
+            this.btnShowNotReady.Text = "ShowNotReady";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelNumber,
+            this.labelName,
+            this.labelStatus});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 49);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(565, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.Margin = new System.Windows.Forms.Padding(0, 1, 20, 2);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(14, 22);
+            this.labelNumber.Text = "#";
+            // 
+            // labelName
+            // 
+            this.labelName.Margin = new System.Windows.Forms.Padding(0, 1, 200, 2);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(39, 22);
+            this.labelName.Text = "Name";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(39, 22);
+            this.labelStatus.Text = "Status";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(73, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(154, 97);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 486);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControlMainWindow.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControlMainWindow;
+        private System.Windows.Forms.TabPage tabPageStatus;
+        private System.Windows.Forms.TabPage tabPageMessage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem menuItemJobs;
+        private System.Windows.Forms.ToolStripMenuItem menuItemOptions;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.ToolStripButton btnDelete;
+        private System.Windows.Forms.ToolStripButton btnShowNotReady;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel labelNumber;
+        private System.Windows.Forms.ToolStripLabel labelName;
+        private System.Windows.Forms.ToolStripLabel labelStatus;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
