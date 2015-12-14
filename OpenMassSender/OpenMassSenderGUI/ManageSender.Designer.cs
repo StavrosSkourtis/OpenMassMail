5b3779dbfbafc0880934ad403e4fe97a0e299e03
thanashs <thanosmichailoudia@gmail.com>
2015-12-14 18:51:37 +0200

    managesender

---
+++ OpenMassSender/OpenMassSenderGUI/ManageSender.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace OpenMassSenderGUI
+{
+    partial class ManageSender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.senderdataGridView = new System.Windows.Forms.DataGridView();
+            this.openMassSenderDBDataSet = new OpenMassSenderGUI.OpenMassSenderDBDataSet();
+            this.senderAccountBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.senderAccountTableAdapter = new OpenMassSenderGUI.OpenMassSenderDBDataSetTableAdapters.SenderAccountTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hostDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddtoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.senderdataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.openMassSenderDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.senderAccountBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddtoolStripButton,
+            this.toolStripLabel1,
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(624, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // senderdataGridView
+            // 
+            this.senderdataGridView.AllowUserToAddRows = false;
+            this.senderdataGridView.AllowUserToDeleteRows = false;
+            this.senderdataGridView.AutoGenerateColumns = false;
+            this.senderdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.senderdataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.firstnameDataGridViewTextBoxColumn,
+            this.lastnameDataGridViewTextBoxColumn,
+            this.hostDataGridViewTextBoxColumn});
+            this.senderdataGridView.DataSource = this.senderAccountBindingSource;
+            this.senderdataGridView.Location = new System.Drawing.Point(0, 28);
+            this.senderdataGridView.Name = "senderdataGridView";
+            this.senderdataGridView.ReadOnly = true;
+            this.senderdataGridView.Size = new System.Drawing.Size(624, 289);
+            this.senderdataGridView.TabIndex = 1;
+            // 
+            // openMassSenderDBDataSet
+            // 
+            this.openMassSenderDBDataSet.DataSetName = "OpenMassSenderDBDataSet";
+            this.openMassSenderDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // senderAccountBindingSource
+            // 
+            this.senderAccountBindingSource.DataMember = "SenderAccount";
+            this.senderAccountBindingSource.DataSource = this.openMassSenderDBDataSet;
+            // 
+            // senderAccountTableAdapter
+            // 
+            this.senderAccountTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.ReadOnly = true;
+            this.emailDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // firstnameDataGridViewTextBoxColumn
+            // 
+            this.firstnameDataGridViewTextBoxColumn.DataPropertyName = "first_name";
+            this.firstnameDataGridViewTextBoxColumn.HeaderText = "first_name";
+            this.firstnameDataGridViewTextBoxColumn.Name = "firstnameDataGridViewTextBoxColumn";
+            this.firstnameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // lastnameDataGridViewTextBoxColumn
+            // 
+            this.lastnameDataGridViewTextBoxColumn.DataPropertyName = "last_name";
+            this.lastnameDataGridViewTextBoxColumn.HeaderText = "last_name";
+            this.lastnameDataGridViewTextBoxColumn.Name = "lastnameDataGridViewTextBoxColumn";
+            this.lastnameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.lastnameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // hostDataGridViewTextBoxColumn
+            // 
+            this.hostDataGridViewTextBoxColumn.DataPropertyName = "host";
+            this.hostDataGridViewTextBoxColumn.HeaderText = "host";
+            this.hostDataGridViewTextBoxColumn.Name = "hostDataGridViewTextBoxColumn";
+            this.hostDataGridViewTextBoxColumn.ReadOnly = true;
+            this.hostDataGridViewTextBoxColumn.Width = 80;
+            // 
+            // AddtoolStripButton
+            // 
+            this.AddtoolStripButton.Image = global::OpenMassSenderGUI.Properties.Resources.add_button;
+            this.AddtoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AddtoolStripButton.Name = "AddtoolStripButton";
+            this.AddtoolStripButton.Size = new System.Drawing.Size(102, 22);
+            this.AddtoolStripButton.Text = "Add New User";
+            this.AddtoolStripButton.Click += new System.EventHandler(this.AddtoolStripButton_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            // 
+            // ManageSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 318);
+            this.Controls.Add(this.senderdataGridView);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "ManageSender";
+            this.Text = "ManageSender";
+            this.Load += new System.EventHandler(this.ManageSender_Load);
+            this.Activated += new System.EventHandler(this.ManageSender_Activated);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.senderdataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.openMassSenderDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.senderAccountBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.DataGridView senderdataGridView;
+        private OpenMassSenderDBDataSet openMassSenderDBDataSet;
+        private System.Windows.Forms.BindingSource senderAccountBindingSource;
+        private OpenMassSenderGUI.OpenMassSenderDBDataSetTableAdapters.SenderAccountTableAdapter senderAccountTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hostDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStripButton AddtoolStripButton;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
